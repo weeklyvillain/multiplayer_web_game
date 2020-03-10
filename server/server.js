e0f72f2c9c9d88ee67b263d3616812f981940266
Filip Eriksson <f.eriksson.21.fe@gmail.com>
2020-03-10 12:44:44 +0100

    basic online functionality completed

---
+++ server/server.js
@@ -0,0 +1,43 @@
+var path = require('path');
+var express = require('express');
+var cors = require('cors')
+
+var app = express();
+const server = require('http').createServer(app);
+
+app.use(express.static(path.join(__dirname, 'public')))
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+  app.options('*', cors())
+app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+  
+var http = require('http').createServer(app);
+
+var server_listen = http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
+
+const io = require('socket.io')(server_listen);
+io.on('connection', client => {
+  console.log('a user connected');
+
+  client.on('join', data => {
+    client.broadcast.emit('add player', data);
+  });
+  
+  client.on('update player', data => { 
+    client.broadcast.emit('player update', data); 
+  });
+  client.on('disconnect', () => { console.log('user disconnected');});
+});
